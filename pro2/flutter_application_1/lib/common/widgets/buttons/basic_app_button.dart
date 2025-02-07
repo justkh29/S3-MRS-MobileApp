0e0f7c5fe09d3e062fbb87d30bd37078da20e5de
Lhn91 <nguyen.le1909@hcmut.edu.vn>
2025-02-07 13:11:57 +0700

    sign_in_fix

--- pro2/flutter_application_1/lib/common/widgets/buttons/basic_app_button.dart
+++ pro2/flutter_application_1/lib/common/widgets/buttons/basic_app_button.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 class BasicAppButton extends StatelessWidget {
+  final Color? backgroundColor; // Màu nền có thể thay đổi
+  final Color? foregroundColor; // Màu chữ có thể thay đổi
   final VoidCallback onPressed;
   final String title;
   final double ? height;
   const BasicAppButton({
+    this.backgroundColor, // Mặc định sẽ dùng màu từ Theme nếu không truyền vào
+    this.foregroundColor,
     required this.onPressed,
     required this.title,
     this.height,
@@ -15,6 +19,8 @@ class BasicAppButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onPressed, 
       style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor ?? Theme.of(context).elevatedButtonTheme.style?.backgroundColor?.resolve({}),
+        foregroundColor: foregroundColor ?? Theme.of(context).elevatedButtonTheme.style?.foregroundColor?.resolve({}),
         minimumSize: Size.fromHeight(height ?? 80),
       ),
       child: Text(
--- pro2/flutter_application_1/lib/presentation/choose_mode/pages/choose_mode.dart
+++ pro2/flutter_application_1/lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/common/widgets/buttons/basic_app_button.dart';
+import 'package:flutter_application_1/core/configs/assets/app_images.dart';
+import 'package:flutter_application_1/core/configs/assets/app_vectors.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_application_1/core/configs/theme/app_colors.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Stack(
+      children: [
+      Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding:EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 120, horizontal: 90),
+                child: Align(alignment: Alignment.topCenter, child: SvgPicture.asset(AppVectors.account)),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: BasicAppButton(onPressed: () {}, title: 'Register'),
+                  ),
+                  SizedBox(width: 20),
+                  Expanded(
+                    flex: 1,
+                    child: BasicAppButton(
+                        backgroundColor: AppColors.primary,
+                        foregroundColor: Colors.white,
+                        onPressed: () {},
+                        title: 'Sign in'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        )
+      ],
+    ));
   }
 }
